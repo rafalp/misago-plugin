ee5c3560601ed4cffddbd51e2a2e8940306aa867
Rafał Pitoń <rafio.xudb@gmail.com>
2023-12-09 13:26:05 +0100

    First pass on plugin

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # misago-plugin
-Barebones Misago plugin used for testing Misago's support for installing plugins from pip
+
+This repo contains a barebones Misago plugin used for testing Misago's support for installing plugins from PYPI through `pip-install.txt` file in `plugins` directory.
---
+++ misago_plugin/misago_plugin.py
@@ -0,0 +1,17 @@
+from misago import MisagoPlugin
+
+
+manifest = MisagoPlugin(
+    name="Misago PIP Plugin",
+    description="This plugin has been installed from PIP through pip-install.txt file.",
+    license="GNU GPL v2",
+    icon="fa-solid fa-download",
+    color="#006dad",
+    version="0.1",
+    author="Rafał Pitoń",
+    homepage="https://misago-project.org",
+    sponsor="https://github.com/sponsors/rafalp",
+    help="https://misago-project.org/c/support/30/",
+    bugs="https://misago-project.org/c/bug-reports/29/",
+    repo="https://github.com/rafalp/misago",
+)
